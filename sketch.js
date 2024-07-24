4dc437b67d6f467f4baa263fb583c7d72e6a75a9
PabloHaddad <pabloeduardomatiashadd@gmail.com>
2024-07-24 20:59:42 -0300

    Adicionando Arquivos

---
+++ sketch.js
@@ -0,0 +1,32 @@
+function verificaColisaoRaquete(x, y) {
+    colidiu = collideRectCircle(x, y, raqueteComprimento, raqueteAltura, xBolinha, Ybolinha, raio);
+    if (colidiu){
+        VelocidadeX *= -1;
+      raquetada.play();
+    }
+}
+
+function calculaChanceDeErrar() {
+  if (pontosDoOponente >= meusPontos) {
+    chanceDeErrar += 1
+    if (chanceDeErrar >= 2){
+    chanceDeErrar = 2
+    }
+  } else {
+    chanceDeErrar -= 1
+    if (chanceDeErrar <= 35){
+    chanceDeErrar = 35
+    }
+  }
+}
+
+
+function bolinhaNaoFicaPresa(){
+    if (xBolinha - raio < 0){
+    XBolinha = 23
+    }
+}
+
+
+
+
